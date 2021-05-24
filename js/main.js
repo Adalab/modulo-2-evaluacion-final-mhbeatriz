22ba0660e744299cab16cd7b5b4b52cd124745b6
Beatriz Mendoza <beatrizmendozahuertas@gmail.com>
2021-05-24 20:25:50 +0200

    fix folders

--- js/main.js
+++ js/main.js
@@ -5,75 +5,101 @@ const btn = document.querySelector(".js-button");
 const listShows = document.querySelector(".js-list");
 const listShowsFav = document.querySelector(".js-listFav");
 const form = document.querySelector(".form");
-
 //VARIABLES
 let shows = [];
 let fav = [];
-
 //LLAMAR A LA API
 function callToApi() {
   fetch(`//api.tvmaze.com/search/shows?q=${inputText.value}`)
     .then((response) => response.json())
     .then((data) => {
+      shows = data; //no estaba guardado los shows en la variable global
       paintShows(data);
+      addListenerToShows(); //cam
     });
 }
-
 //PINTAR SERIES
+// function paintShows(data) {
+//   for (let i = 0; i < data.length; i++) {
+//     //console.log(data[i].show);
+//     const dataList = data[i].show;
+//     //buscar si la paleta que se está pintando esta en favoritos
+//     const isPresent = fav.find((favoriteId) => favoriteId.show.id === dataList.id);
+//     let classFavorite = "";
+//     if (isPresent === undefined) {
+//       classFavorite = "";
+//     } else {
+//       classFavorite = "favorite";
+//     }
+//     if (dataList.image === null) {
+//       listShows.innerHTML += `
+//             <li data-id="${dataList.id}" class="js-card">
+//             <h2>${dataList.name}</h2>
+//             <img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/>
+//              </li>`;
+//     } else {
+//       listShows.innerHTML += `
+//         <li data-id="${dataList.id}" class="js-card">
+//         <h2>${dataList.name}</h2>
+//         <img src="${dataList.image.medium}"/>
+//          </li>`;
+//     }
+//   }
+// }
 function paintShows(data) {
+  listShows.innerHTML = ``;
+  if (data.length === 0) {
+    listShows.innerHTML = `<li><p>vacio</p></li>`;
+    return;
+  }
   for (let i = 0; i < data.length; i++) {
     console.log(data[i].show);
     const dataList = data[i].show;
-    //buscar si la paleta que se está pintando esta en favoritos
-    // const isPresent = fav.find((favoriteId) => favoriteId === dataList.id);
-    // let classFavorite = "";
-    // if (isPresent === undefined) {
-    //   classFavorite = "";
-    // } else {
-    //   classFavorite = "favorite";
-    // }
-
-    if (dataList.image === null) {
+    //faltaban condicionales
+    if (dataList.image) {
       listShows.innerHTML += `
-            <li data-id="${dataList.id}" class="js-card">
+          <li data-id="${dataList.id}" class="js-card">
             <h2>${dataList.name}</h2>
-            <img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/>
-             </li>`;
+            <img src="${dataList.image.medium}"/>
+          </li>`;
     } else {
       listShows.innerHTML += `
-        <li data-id="${dataList.id}" class="js-card">
-        <h2>${dataList.name}</h2>
-        <img src="${dataList.image.medium}"/>
-         </li>`;
+          <li data-id="${dataList.id}" class="js-card">
+            <h2>${dataList.name}</h2>
+             <img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/>
+           </li>`;
     }
-
-    // if (dataList.image === null) {
-    //   listShowsFav.innerHTML += `
-    //         <li data-id="${dataList.id}" class="js-card favorite">
-    //         <h2>${dataList.name}</h2>
-    //         <img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/>
-    //          </li>`;
-    // } else {
-    //   listShows.innerHTML += `
-    //     <li data-id="${dataList.id}" class="js-card favorite">
-    //     <h2>${dataList.name}</h2>
-    //     <img src="${dataList.image.medium}"/>
-    //      </li>`;
-    // }
   }
-  addListenerToShows();
 }
-
+function paintShowsFav(data) {
+  // antes este data era un array de números y ahora llega un array de objetos
+  listShowsFav.innerHTML = ``;
+  for (let i = 0; i < data.length; i++) {
+    const dataList = data[i].show;
+    //faltaban condicionales
+    if (dataList.image) {
+      listShowsFav.innerHTML += `
+              <li data-id="${dataList.id}" class="js-card">
+                <h2>${dataList.name}</h2>
+                <img src="${dataList.image.medium}"/>
+              </li>`;
+    } else {
+      listShowsFav.innerHTML += `
+              <li data-id="${dataList.id}" class="js-card">
+                  <h2>${dataList.name}</h2>
+                  <img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"/>
+                  </li>`;
+    }
+  }
+}
 //FUNCIÓN MANEJADORA
 function handlerClick(event) {
   event.preventDefault();
   callToApi();
 }
-
 //EVENTO
 form.addEventListener("submit", handlerClick);
 btn.addEventListener("click", handlerClick);
-
 //FAVORITAS
 function addListenerToShows() {
   const listShowsFav = document.querySelectorAll(".js-card");
@@ -82,23 +108,20 @@ function addListenerToShows() {
   }
 }
 function handlerClickFav(event) {
-  //identificar la li pulsada
+  //IDENTIFICAR LA LI PULSADA
   const showCardFav = event.currentTarget;
   showCardFav.classList.toggle("favorite");
-
-  //obtener info asociada al array fav
-  const showId = showCardFav.dataset.id;
-
-  //buscar si la img clickada esta en favoritos
-  const isPresent = fav.find((favoriteId) => favoriteId === showId);
+  //OBTENER INFO ASOCIADA AL ARRAY FAV
+  const showId = parseInt(showCardFav.dataset.id);
+  //BUSCAR SI LA IMG CLICkADA ESTA EN FAV
+  const show = shows.find((s) => s.show.id === showId);
+  if (show === undefined) return;
+  const isPresent = fav.find((favorite) => favorite.show.id === showId); //
   if (isPresent === undefined) {
-    fav.push(parseInt(showId));
+    fav.push(show); // empujaba número no el id, ahora empuja objeto show
   } else {
-    fav = fav.filter((favoriteId) => favoriteId !== parseInt(showId));
+    fav = fav.filter((favoriteId) => favorite.show.id !== showId);
   }
-  console.log(fav);
+  paintShowsFav(fav); // no llamaba  a la función
 }
-
 addListenerToShows();
-
-// RESET
